60f9633527af5909937807e8f6adf5c1e90b9a8c
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-08-09 05:52:53 +0300

    Write Fibonacci recursion method

--- fibonacci.js
+++ fibonacci.js
@@ -9,3 +9,14 @@ function fibs(n) {
 }
 
 console.log(fibs(12));
+
+function fibsRec(n, array = [0, 1]) {
+  if (array.length == n) {
+    return array;
+  } else {
+    array.push(array[array.length - 1] + array[array.length - 2]);
+    return fibsRec(n, array);
+  }
+}
+
+console.log(fibsRec(8));
